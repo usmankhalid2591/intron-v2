83b2639b97a38ff4f68cac7d263abc02cb6ba2dc
Usman Khalid <44892740+usmankhalid2591@users.noreply.github.com>
2021-04-11 15:08:29 +0500

    Changes.

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body");
+let sideNav = document.getElementById("sideNav");
+
+if (!!sideNav) {
+  body.style.paddingLeft = "315px";
+  body.style.paddingRight = "15px";
+  body.style.paddingTop = "100px";
+}

ff57acdf2dbc6e5d3d9d3fe429cc140f51ddac24
Usman Khalid <44892740+usmankhalid2591@users.noreply.github.com>
2021-04-12 03:13:57 +0500

    Add Staff

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,33 @@
 let body = document.querySelector("body");
 let sideNav = document.getElementById("sideNav");
 
-if (!!sideNav) {
-  body.style.paddingLeft = "315px";
-  body.style.paddingRight = "15px";
-  body.style.paddingTop = "100px";
-}
+window.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth > 768) {
+    if (!!sideNav) {
+      body.style.paddingLeft = "315px";
+      body.style.paddingRight = "15px";
+    }
+  } else {
+    body.style.paddingRight = "30px";
+    body.style.paddingLeft = "30px";
+  }
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    if (!!sideNav) {
+      body.style.paddingLeft = "315px";
+      body.style.paddingRight = "15px";
+    }
+  } else {
+    body.style.paddingRight = "30px";
+    body.style.paddingLeft = "30px";
+  }
+});
+
+let sideNavToggler = document.getElementById("sideNavToggler");
+
+sideNavToggler.addEventListener("click", function () {
+  sideNav.classList.toggle("side-nav-show");
+  sideNavToggler.classList.toggle("side-nav-toggler-position");
+});
